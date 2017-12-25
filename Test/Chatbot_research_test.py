a27cf76f5176966ea22a88a708f4b53ab7043c15
tatatakky <s1240232@gmail.com>
2017-12-25 18:31:11 +0900

    [add] Third commit

--- Test/Chatbot_research_test.py
+++ Test/Chatbot_research_test.py
@@ -6,7 +6,6 @@ def AI_reserch_from_dictionary(initial_word):
     html = requests.get("http://tokoton-eitango.com/eitango/wordindex/"+initial_word)
     soup = BeautifulSoup(html.text,"html.parser")
     string_include_tag = soup.findAll("td",style="width: 50%; text-align: left; padding-left: 10px;")
-    for i in range(len(string_include_tag )):
+    for i in range(len(string_include_tag)):
         print(string_include_tag[i].text)
-
 AI_reserch_from_dictionary("B")
